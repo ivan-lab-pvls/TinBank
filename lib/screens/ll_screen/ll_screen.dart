c15d449566af38f7e2c2c97e653308fe854c59d3
ilobosok <121606196+ivan-lab-pvls@users.noreply.github.com>
2023-10-22 14:10:12 +0200

    first commitxx

--- lib/screens/ll_screen/ll_screen.dart
+++ lib/screens/ll_screen/ll_screen.dart
@@ -1,6 +1,6 @@
 import 'package:ti_bank/custom_theme.dart';
 import 'package:flutter/material.dart';
-// import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 class LLScreen extends StatelessWidget {
   const LLScreen({super.key, required this.u});
@@ -13,14 +13,14 @@ class LLScreen extends StatelessWidget {
         backgroundColor: CustomTheme.whiteColor,
         iconTheme: const IconThemeData(color: CustomTheme.blackColor),
       ),
-      // body: SafeArea(
-      //   bottom: false,
-      //   child: InAppWebView(
-      //     initialUrlRequest: URLRequest(
-      //       url: Uri.parse(u),
-      //     ),
-      //   ),
-      // ),
+      body: SafeArea(
+        bottom: false,
+        child: InAppWebView(
+          initialUrlRequest: URLRequest(
+            url: Uri.parse(u),
+          ),
+        ),
+      ),
     );
   }
 }
